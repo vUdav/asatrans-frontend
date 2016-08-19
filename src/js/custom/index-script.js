2f0f1d80106c4f9c91120315211d87ebfad4caaa
vUdav <v.udav@bk.ru>
2016-08-19 17:15:20 +0400

    add about

--- src/js/custom/index-script.js
+++ src/js/custom/index-script.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
 		app.promoSlider(); // промо каруселька
 		// app.feedbackDatepicker(); // дейтпикер
 		app.feedbackRange();
+		app.abountCounter();
+
 	};
 
 	app.promoSlider = function(){
@@ -32,6 +34,29 @@ $(document).ready(function() {
 	app.feedbackRange = function(){
 		var el = $(".js-feedback-range");
 		el.slider();
+	};
+
+	app.abountCounter = function(){
+		var options = {
+			useEasing : true,
+			useGrouping : true,
+			separator : '',
+			decimal : '',
+			prefix : '',
+			suffix : ''
+		};
+		var ordersStartCount = document.querySelector('#js-counter-orders').innerText;
+		var regStartCount = document.querySelector('#js-counter-reg').innerText;
+		var dirStartCount = document.querySelector('#js-counter-dir').innerText;
+		var distanceStartCount = document.querySelector('#js-counter-distance').innerText;
+		var orders = new CountUp("js-counter-orders", 0, ordersStartCount, 0, 5, options);
+		var reg = new CountUp("js-counter-reg", 0, regStartCount, 0, 5, options);
+		var dir = new CountUp("js-counter-dir", 0, dirStartCount, 0, 5, options);
+		var distance = new CountUp("js-counter-distance", 0, distanceStartCount, 0, 5, options);
+		orders.start();
+		reg.start();
+		dir.start();
+		distance.start();
 	};
 
 	app.init();
